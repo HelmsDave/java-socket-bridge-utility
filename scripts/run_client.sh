1a980d9229ecafd2d67de3b51993871b9c338a00
Dave Helms <dave.helms.home@gmail.com>
2021-09-19 10:37:08 -0500

    updated ec2 ip

--- scripts/run_client.sh
+++ scripts/run_client.sh
@@ -3,5 +3,5 @@
 java -cp ../target/java-socket-bridge-utility-1.0-SNAPSHOT.jar \
    org.harmonograph.socket.bridge.SockClientRelayMain \
    --pullHost localhost --pullSock 30003 \
-   --pushHost localhost --pushSock 30004 \
+   --pushHost 3.84.218.184 --pushSock 30004 \
    --verbose
