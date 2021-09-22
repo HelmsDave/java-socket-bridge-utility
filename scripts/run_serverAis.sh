154fb788914f07f497e52023fe2258d1d05dd4eb
Dave Helms <dave.helms.home@gmail.com>
2021-09-22 08:35:52 -0500

    fixed port numbers

--- scripts/run_serverAis.sh
+++ scripts/run_serverAis.sh
@@ -2,6 +2,6 @@
 
 java -cp ../target/java-socket-bridge-utility-1.0-SNAPSHOT.jar \
    org.harmonograph.socket.server.SockServerRelayMain \
-   -uplinkPort 30004 \
-   -downlinkPort 30003 \
+   -uplinkPort 30009 \
+   -downlinkPort 30008 \
    -verbose
