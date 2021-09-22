6d08a076a9f3969ec8ceadfddffc8bfa1af3ab9f
Dave Helms <dave.helms.home@gmail.com>
2021-09-22 08:04:38 -0500

    fixed port flags

--- scripts/run_clientAdsb.sh
+++ scripts/run_clientAdsb.sh
@@ -2,6 +2,6 @@
 
 java -cp /home/pi/java-socket-bridge-utility/target/java-socket-bridge-utility-1.0-SNAPSHOT.jar \
    org.harmonograph.socket.bridge.SockClientRelayMain \
-   -pullHost localhost -pullSock 30003 \
-   -pushHost 3.84.218.184 -pushSock 30004 \
+   -pullHost localhost -pullPort 30003 \
+   -pushHost 3.84.218.184 -pushPort 30004 \
    -verbose
--- scripts/run_clientAis.sh
+++ scripts/run_clientAis.sh
@@ -2,7 +2,7 @@
 
 java -cp /home/pi/java-socket-bridge-utility/target/java-socket-bridge-utility-1.0-SNAPSHOT.jar \
    org.harmonograph.socket.bridge.SockClientRelayMain \
-   -pullHost localhost -pullSock 30008 \
-   -pushHost 3.84.218.184 -pushSock 30009 \
+   -pullHost localhost -pullPort 30008 \
+   -pushHost 3.84.218.184 -pushPort 30009 \
    -verbose
 
