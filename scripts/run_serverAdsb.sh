7d7c88e326c8efc28ef6185008b6f6b339b5dc1e
Dave Helms <dave.helms.home@gmail.com>
2021-09-23 20:47:57 -0500

    merged server scripts and added log file

--- scripts/run_serverAdsb.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-java -cp ../target/java-socket-bridge-utility-1.0-SNAPSHOT.jar \
-   org.harmonograph.socket.server.SockServerRelayMain \
-   -uplinkPort 30004 \
-   -downlinkPort 30003 \
-   -verbose
