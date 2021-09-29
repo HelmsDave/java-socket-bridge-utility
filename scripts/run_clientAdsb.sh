fa657e22cb0be96db2687b12d518129def9a3188
Dave Helms <dave.helms.home@gmail.com>
2021-09-28 21:11:41 -0500

    Revert "updated client run scripts"
    This reverts commit f30a925c13a8e8875dd1ed95f07154b8095266bd.
    
    Revert "updated client run script"
    
    This reverts commit 0bbfb85ad002a1f072828640244b7217679a5e34.

---
+++ scripts/run_clientAdsb.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+java -cp /home/pi/java-socket-bridge-utility/target/java-socket-bridge-utility-1.0-SNAPSHOT.jar \
+   org.harmonograph.socket.bridge.SockClientRelayMain \
+   -pullHost localhost -pullPort 30003 \
+   -pushHost 3.84.218.184 -pushPort 30004 \
+   -verbose
