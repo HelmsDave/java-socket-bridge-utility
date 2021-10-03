f0f2906ee9555a4022939cdb2229d5310944395b
Dave Helms <dave.helms.home@gmail.com>
2021-10-02 19:49:22 -0500

    added system init scripts for server

---
+++ scripts/run_serverAis.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+java -cp ../target/java-socket-bridge-utility-1.0-SNAPSHOT.jar \
+   org.harmonograph.socket.server.SockServerRelayMain \
+   -uplinkPort 30009 \
+   -downlinkPort 30008 \
+   -verbose
+   
