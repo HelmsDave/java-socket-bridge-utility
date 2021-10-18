a7aa7d2e114e6a175ae1d5479fddf3bc6fa6a2f2
Dave Helms <dave.helms.home@gmail.com>
2021-10-18 17:36:22 -0500

    fixed scripts for better shutdown

--- server/scripts/run_serverAdsb2.sh
+++ server/scripts/run_serverAdsb2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-java -cp "/home/ec2-user/java-socket-bridge-utility/server/target/java-socket-server-utility-1.0-SNAPSHOT.jar:/home/ec2-user/java-socket-bridge-utility/server/target/deps/*" \
+exec java -cp "/home/ec2-user/java-socket-bridge-utility/server/target/java-socket-server-utility-1.0-SNAPSHOT.jar:/home/ec2-user/java-socket-bridge-utility/server/target/deps/*" \
    org.harmonograph.socket.server.SockServerRelayMain \
    -uplinkPort 30014 \
    -downlinkPort 30013 \
